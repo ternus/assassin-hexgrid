32763caabf3a778cbba7126fc3865f07cb63aa01
Christian Ternus <ternus@mit.edu>
2009-01-16 20:13:46 -0500

    jobs

---
+++ archives/jobs.py
@@ -0,0 +1,10 @@
+from turbogears.scheduler import add_weekday_task
+from archives.model import autotick
+from turbogears.database import PackageHub
+
+hub = PackageHub("archives") 
+#con = connectionForURI(hub.uri)
+__connection__ = hub
+
+def schedule():
+    add_weekday_task(action=autotick, weekdays=(1,3,4,5,6,7), timeonday=(16, 58))
